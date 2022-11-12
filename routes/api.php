11a01772f129fa7265daeab2ea081f57812d859d
Mahmudul Hasan <mahmudul.ruet12@gmail.com>
2022-11-12 22:18:36 +0100

    models and migrations are created

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/test', function(Request $request){
+    $pieces = explode(",", $request->productId);
+
+    print_r($pieces);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
